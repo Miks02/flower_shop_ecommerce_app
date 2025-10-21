ada493952ced192ac45ae596dbbe657062f8b65a
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-21 20:49:49 +0200

    feat(ComponentViewModel): Add HeroViewModel

---
+++ FlowerShop/ViewModels/Components/HeroViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlowerShop.ViewModels.Components;
+
+public class HeroViewModel
+{
+    public string Title { get; set; } = null!;
+    public string? Subtitle { get; set; } = null!;
+    public string BackgroundImage { get; set; } = null!;
+    public string IsHome { get; set; } = null!;
+}
