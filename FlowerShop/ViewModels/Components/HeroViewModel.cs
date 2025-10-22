d515b9c3fe626f4df93255f3c59fef42036a6bcf
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-22 15:52:46 +0200

    refactor(ComponentViewModel): Change IsHome type from string to bool

--- FlowerShop/ViewModels/Components/HeroViewModel.cs
+++ FlowerShop/ViewModels/Components/HeroViewModel.cs
@@ -5,5 +5,5 @@ public class HeroViewModel
     public string Title { get; set; } = null!;
     public string? Subtitle { get; set; } = null!;
     public string BackgroundImage { get; set; } = null!;
-    public string IsHome { get; set; } = null!;
+    public bool IsHome { get; set; }
 }
