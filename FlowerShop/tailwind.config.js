868bb2e17c4bf89b4a15201e0bd5d50f47c369da
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-23 13:28:51 +0200

    chore(dependencies): Commit json config files

--- FlowerShop/tailwind.config.js
+++ FlowerShop/tailwind.config.js
@@ -1,20 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        // Glavni Views folder
         './Views/**/*.cshtml',
 
-        // Views u Areas (ako ih koristiš)
         './Areas/**/Views/**/*.cshtml',
 
-        // Layout fajlovi (ako ih imaš u Shared)
         './Views/Shared/**/*.cshtml',
 
-        // Static HTML fajlovi ako ih imaš (npr. landing page)
         './wwwroot/**/*.html',
 
-        // JS fajlovi (ako koristiš Tailwind klase unutar JS-a)
         './wwwroot/js/**/*.js',
+        
     ],
     theme: {
         extend: {},
---
+++ FlowerShop/wwwroot/js/main.js
@@ -0,0 +1,1 @@
+console.log("Typess rarrrss rsdsdssarrsdss");
