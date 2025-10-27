7d5f71c5e68f7ed1ce6596e5bce61434ec687742
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-10-27 14:15:50 +0100

    feat(dbcontext): Add missing DbSets into the DbContext class

--- FlowerShop/Data/ApplicationDbContext.cs
+++ FlowerShop/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using FlowerShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
@@ -13,13 +14,21 @@ public class ApplicationDbContext :  IdentityDbContext<ApplicationUser>
         
     }
     
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<FlowerType> FlowerTypes { get; set; }
+    public DbSet<Occasion> Occasions { get; set; }
+    public DbSet<ProductFlower> ProductFlowers { get; set; }
+    
      protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
+     {
+         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
             
-            builder.Entity<ApplicationUser>()
-                .Property(u => u.PhoneNumber)
-                .IsRequired();
+         base.OnModelCreating(builder);
+            
+         builder.Entity<ApplicationUser>()
+             .Property(u => u.PhoneNumber)
+             .IsRequired();   
 
         }
 }
