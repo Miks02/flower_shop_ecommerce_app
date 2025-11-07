d54bd0bcb648d403e13010219de96fa43920f9a8
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-11-07 02:34:35 +0100

    feat(mock): Add more data to the MockProductService

--- FlowerShop/Services/Mock/MockProductService.cs
+++ FlowerShop/Services/Mock/MockProductService.cs
@@ -26,7 +26,23 @@ public class MockProductService : IProductService
 
         _products = new List<Product>()
         {
-            new Product { Id = 1, Name = "Buket crvenih ruža", Price = 999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Buket crvenih ruza.jpg", Category = _categories[0], Occasions = _occasions }
+            new Product { Id = 1, Name = "Ljubavno Crveno", Price = 5999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Buket crvenih ruza.jpg", Category = _categories[0], Occasions = _occasions },
+            new Product { Id = 2, Name = "Nebeska Magija", Price = 9999, Stock = 100, Description = "Buket za svadbe i rodjenje deteta", ImageUrl = "./AppImages/Proizvodi/Buketi/Buket Sky.jpg", Category = _categories[0], Occasions = _occasions },
+            new Product { Id = 3, Name = "Tropska Romansa", Price = 6999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Buket Tropska Romansa.jpg", Category = _categories[0], Occasions = _occasions },
+            new Product { Id = 4, Name = "Osmeh Suncokreta", Price = 999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Buket Osmeh Suncokreta.jpg", Category = _categories[0], Occasions = _occasions },
+            new Product { Id = 5, Name = "Romantični Raj", Price = 999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Buket Romantični Raj.jpg", Category = _categories[0], Occasions = _occasions },
+            new Product { Id = 6, Name = "Crveno Carstvo", Price = 1999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Crveno Carstvo.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 7, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 8, Name = "Buket crvenih ruža", Price = 999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Orhideje i Mini Ruže.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 9, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 10, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 11, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 12, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 13, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 14, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 15, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            new Product { Id = 16, Name = "Ruže i Lizijantus", Price = 4999, Stock = 100, Description = "Buket za sve prilike", ImageUrl = "./AppImages/Proizvodi/Buketi/Ruže i Lizijantus.jpg", Category = _categories[1], Occasions = _occasions },
+            
 
         };
     }
@@ -36,4 +52,6 @@ public class MockProductService : IProductService
         return Task.FromResult(_products.AsEnumerable());
         
     }
+
+
 }
