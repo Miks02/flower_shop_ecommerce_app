afd8cbc0bc90546cd7625c08ae29b59c232f4ce3
Miks02 <milan.miki.nikolic2002@gmail.com>
2025-11-13 16:00:28 +0100

    feat(view): Add role-based rendering for authentication and profile menus

--- FlowerShop/Components/ProfileMenuViewComponent.cs
+++ FlowerShop/Components/ProfileMenuViewComponent.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlowerShop.Components;
 
+[Authorize(Roles = "User")]
 public class ProfileMenuViewComponent : ViewComponent
 {
     public async Task<IViewComponentResult> InvokeAsync()
