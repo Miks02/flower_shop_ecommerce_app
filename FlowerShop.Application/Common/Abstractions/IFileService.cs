74ab679ee1f34edc7bed40f8181231df46b94dde
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-04-15 12:48:59 +0200

    fix(file-service): Add missing methods in IFileService

--- FlowerShop.Application/Common/Abstractions/IFileService.cs
+++ FlowerShop.Application/Common/Abstractions/IFileService.cs
@@ -1,6 +1,10 @@
+using FlowerShop.SharedKernel.Results;
+using Microsoft.AspNetCore.Http;
+
 namespace FlowerShop.Application.Common.Abstractions;
 
 public interface IFileService
 {
-    
+    Task<Result<string>> UploadFile(IFormFile file, string? uploadedFilePath, string? uploadSubDir);
+    public Task<Result> DeleteFile(string filePath);
 }
