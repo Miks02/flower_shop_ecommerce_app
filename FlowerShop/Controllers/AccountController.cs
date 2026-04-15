04babeed8f28637734eb7f8cfe15f67ab35d1a11
Miks02 <milan.miki.nikolic2002@gmail.com>
2026-04-15 11:36:27 +0200

    feat(logout): Add LogoutHandler and replace it with signInManager in AccountController

--- FlowerShop/Controllers/AccountController.cs
+++ FlowerShop/Controllers/AccountController.cs
@@ -1,4 +1,8 @@
 using System.Security.Claims;
+using Azure;
+using FlowerShop.Application.Features.Auth.Commands.Login;
+using FlowerShop.Application.Features.Auth.Commands.Logout;
+using FlowerShop.Application.Features.Auth.Commands.RegisterUser;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using FlowerShop.Web.Models;
@@ -10,27 +14,30 @@ namespace FlowerShop.Web.Controllers;
 
  public class AccountController : BaseController
     {
-        private readonly SignInManager<ApplicationUser> _signInManager;
-        private readonly IUserService _userService;
+        private readonly RegisterUserHandler _registerUserHandler;
+        private readonly LoginHandler _loginHandler;
+        private readonly LogoutHandler _logoutHandler;
 
         private readonly string _loginComponent = "Components/Login/Default";
         private readonly string _registerComponent = "Components/Register/Default";
         private readonly string _profileSettingsComponent = "~/Views/Shared/Components/Settings/Default.cshtml";
 
         public AccountController(
-            SignInManager<ApplicationUser> signInManager, 
-            IUserService userService,
-            ILogger<BaseController> logger
+            ILogger<BaseController> logger,
+            LoginHandler loginHandler,
+            RegisterUserHandler registerUserHandler,
+            LogoutHandler logoutHandler
             ) : base(logger)
         {
-            _signInManager = signInManager;
-            _userService = userService;
+            _registerUserHandler = registerUserHandler;
+            _loginHandler = loginHandler;
+            _logoutHandler = logoutHandler;
         }
 
         [HttpGet]
         public IActionResult Login()
         {
-            if (!_signInManager.IsSignedIn(User))
+            if (!User.Identity!.IsAuthenticated)
                 return PartialView(_loginComponent);
             
             Response.Headers.Append("HX-Redirect", "/Home/Index");
@@ -46,8 +53,15 @@ namespace FlowerShop.Web.Controllers;
                 return PartialView(_loginComponent, model);
             }
 
-            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
-            if (result.Succeeded)
+            var loginCommand = new LoginCommand
+            {
+                Username = model.Username,
+                Password = model.Password,
+                RememberMe = model.RememberMe
+            };
+
+            var result = await _loginHandler.Handle(loginCommand);
+            if (result.IsSuccess)
             {
                 Response.Headers.Append("HX-Redirect", "/Home/Index");
 
@@ -63,7 +77,7 @@ namespace FlowerShop.Web.Controllers;
         [HttpGet]
         public IActionResult Register()
         {
-            if (!_signInManager.IsSignedIn(User))
+            if (!User.Identity!.IsAuthenticated)
                 return PartialView(_registerComponent);
 
             Response.Headers.Append("HX-Redirect", "/Home/Index");
@@ -77,7 +91,18 @@ namespace FlowerShop.Web.Controllers;
             if (!ModelState.IsValid) 
                 return PartialView(_registerComponent, model);
 
-            var result = await _userService.CreateUserAsync(model, "User");
+            var registerCommand = new RegisterUserCommand
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                Password = model.Password,
+                Username = model.Username,
+                ConfirmPassword = model.ConfirmPassword,
+                PhoneNumber = model.PhoneNumber,
+                Email = model.Email
+            };
+
+            var result = await _registerUserHandler.Handle(registerCommand);
 
             if (!result.IsSuccess)
             {
@@ -85,81 +110,79 @@ namespace FlowerShop.Web.Controllers;
                     ModelState.AddModelError(nameof(model.ConfirmPassword), error.Description);
                 return PartialView(_registerComponent, model);
             }
-            
-            await _signInManager.SignInAsync(result.Payload!, isPersistent: false);
+
+            await _loginHandler.Handle(new LoginCommand { Username = model.Username, Password = model.Password });
             
             Response.Headers.Append("HX-Redirect", "/Home/Index");
             return Ok();
         }
         
 
-        [Authorize]
-        [HttpPost]
-        public async Task<IActionResult> UpdateProfile(SettingsPageViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return PartialView(_profileSettingsComponent, model);
-
-            var result = await _userService.UpdateProfileAsync(model);
-            
-            if (!result.IsSuccess)
-            {
-                string errorMessage = string.Empty;
-                foreach (var error in result.Errors!)
-                    errorMessage += " " + error.Description;
-                
-                SetErrorMessage(errorMessage);
-                return PartialView(_profileSettingsComponent, model);
-            }
-            
-            string message = (result.Payload!.ProfileUpdated, result.Payload.PasswordChanged) switch
-            {
-                (false, false) => "Nema izmenjenih podataka za čuvanje.",
-                (true,  false) => "Profil je uspešno ažuriran.",
-                (false, true ) => "Lozinka je uspešno ažurirana.",
-                (true,  true ) => "Profil i lozinka su uspešno ažurirani."
-            };
-            
-            if(result.Payload.ProfileUpdated || result.Payload.PasswordChanged)
-                await _signInManager.RefreshSignInAsync(result.Payload.User);
-            
-            SetSuccessMessage(message);
-            Response.Headers.Append("HX-Redirect", "/User/Profile/Settings");
-            return Ok();
-        }
-
-        [Authorize]
-        [HttpGet]
-        public async Task<IActionResult> RemoveProfilePicture()
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            if (userId is null)
-            {
-                SetErrorMessage("Došlo je do neočekivane greške. Korisnik nije pronadjen");
-                return ViewComponent("Settings");
-            }
-
-            var result = await _userService.RemoveProfilePictureAsync(userId);
-
-            if (!result.IsSuccess)
-            {
-                SetErrorMessage("Došlo je do greške prilikom brisanja profilne slike");
-                return ViewComponent("Settings");
-            }
-            
-            SetSuccessMessage("Profilna slika je uspešno obrisana!");
-            return ViewComponent("Settings");
-
-        }
+        // [Authorize]
+        // [HttpPost]
+        // public async Task<IActionResult> UpdateProfile(SettingsPageViewModel model)
+        // {
+        //     if (!ModelState.IsValid)
+        //         return PartialView(_profileSettingsComponent, model);
+        //
+        //     var result = await _userService.UpdateProfileAsync(model);
+        //     
+        //     if (!result.IsSuccess)
+        //     {
+        //         string errorMessage = string.Empty;
+        //         foreach (var error in result.Errors!)
+        //             errorMessage += " " + error.Description;
+        //         
+        //         SetErrorMessage(errorMessage);
+        //         return PartialView(_profileSettingsComponent, model);
+        //     }
+        //     
+        //     string message = (result.Payload!.ProfileUpdated, result.Payload.PasswordChanged) switch
+        //     {
+        //         (false, false) => "Nema izmenjenih podataka za čuvanje.",
+        //         (true,  false) => "Profil je uspešno ažuriran.",
+        //         (false, true ) => "Lozinka je uspešno ažurirana.",
+        //         (true,  true ) => "Profil i lozinka su uspešno ažurirani."
+        //     };
+        //     
+        //     if(result.Payload.ProfileUpdated || result.Payload.PasswordChanged)
+        //         await _signInManager.RefreshSignInAsync(result.Payload.User);
+        //     
+        //     SetSuccessMessage(message);
+        //     Response.Headers.Append("HX-Redirect", "/User/Profile/Settings");
+        //     return Ok();
+        // }
+        //
+        // [Authorize]
+        // [HttpGet]
+        // public async Task<IActionResult> RemoveProfilePicture()
+        // {
+        //     var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        //
+        //     if (userId is null)
+        //     {
+        //         SetErrorMessage("Došlo je do neočekivane greške. Korisnik nije pronadjen");
+        //         return ViewComponent("Settings");
+        //     }
+        //
+        //     var result = await _userService.RemoveProfilePictureAsync(userId);
+        //
+        //     if (!result.IsSuccess)
+        //     {
+        //         SetErrorMessage("Došlo je do greške prilikom brisanja profilne slike");
+        //         return ViewComponent("Settings");
+        //     }
+        //     
+        //     SetSuccessMessage("Profilna slika je uspešno obrisana!");
+        //     return ViewComponent("Settings");
+        //
+        // }
 
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
-            await _signInManager.SignOutAsync();
+            await _logoutHandler.Handle();
             return RedirectToAction("Index", "Home");
         }
-
-        [HttpGet]
-        public IActionResult AccessDenied() => View();
+        
     }
