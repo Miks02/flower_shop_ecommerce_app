6c4365fdb6e835777e041f1ca7834b66cb273d55
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-06-07 15:43:09 +0200

    feat(unit-of-work): Add transaction management methods to IUnitOfWork and implementation

--- FlowerShop.Infrastructure/Persistence/Repositories/UnitOfWork.cs
+++ FlowerShop.Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -1,10 +1,37 @@
+using System.Data;
 using FlowerShop.Application.Common.Abstractions;
 using FlowerShop.Infrastructure.Persistence.EntityFramework;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlowerShop.Infrastructure.Persistence.Repositories;
 
 public class UnitOfWork(AppDbContext context) : IUnitOfWork
 {
+    private IDbContextTransaction? _transaction;
+    
     public async Task<int> SaveAsync(CancellationToken ct) 
         => await context.SaveChangesAsync(ct);
+
+    public async Task BeginTransactionAsync(CancellationToken ct = default)
+        => _transaction = await context.Database.BeginTransactionAsync(IsolationLevel.RepeatableRead, ct);
+
+    public async Task CommitAsync(CancellationToken ct = default)
+    {
+        if (_transaction is null)
+            throw new InvalidOperationException("Transaction has not been started. Call BeginTransactionAsync() before committing.");
+        
+        await _transaction.CommitAsync(ct);
+        await _transaction.DisposeAsync();
+        _transaction = null;
+    }
+
+    public async Task RollbackAksync(CancellationToken ct = default)
+    {
+        if (_transaction is null)
+            return;
+        await _transaction.RollbackAsync(ct);
+        await _transaction.DisposeAsync();
+        _transaction = null;
+    }
 }
