daa323edc84563bfb8de362e95bdb2d3dc0105c4
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-02 12:31:36 +0200

    feat(deliverers): Add display names for DelivererStatus and VehicleType enums

--- FlowerShop.Domain/Entities/Deliverers/DelivererStatus.cs
+++ FlowerShop.Domain/Entities/Deliverers/DelivererStatus.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FlowerShop.Domain.Entities.Deliverers;
 
 public enum DelivererStatus
 {
+    [Display(Name = "Dostupan")]
     Available,
+    [Display(Name = "Na dostavi")]
     OnDuty,
+    [Display(Name = "Nedostupan")]
     Unavailable
 }
