fe167ac567e0a4d8fba2531bc7fc28533915523e
Milan Nikolić <milan.miki.nikolic2002@gmail.com>
2026-09-08 10:52:16 +0200

    feat(reviews): Implement CreateReview command and handler for review submissions

---
+++ FlowerShop.Application/Features/Reviews/Commands/CreateReview/CreateReviewHandler.cs
@@ -0,0 +1,42 @@
+using FlowerShop.Application.Common.Abstractions;
+using FlowerShop.Domain.Entities.Orders;
+using FlowerShop.Domain.Entities.Reviews;
+using FlowerShop.SharedKernel.Results;
+
+namespace FlowerShop.Application.Features.Reviews.Commands.CreateReview;
+
+public class CreateReviewHandler(
+    IReviewRepository reviewRepo, 
+    IOrderRepository orderRepo,
+    IUnitOfWork unitOfWork) : IHandler
+{
+    public async Task<Result> Handle(CreateReviewCommand command, CancellationToken ct = default)
+    {
+        var orderToReview = await orderRepo.GetByIdAsync(command.OrderId, ct);
+        
+        if(orderToReview is null)
+            return Result.Failure(OrderError.OrderNotFound(command.OrderId.ToString()));
+
+        if (orderToReview.UserId != command.ReviewerId)
+            return Result.Failure(ReviewError.NotYourOrder(orderToReview.OrderNumber));
+
+        if (orderToReview.DeliveryStatus != DeliveryStatus.Delivered)
+            return Result.Failure(ReviewError.NotReadyToReview(orderToReview.OrderNumber));
+        
+        if(orderToReview.ReviewId is not null)
+            return Result.Failure(ReviewError.AlreadyReviewed(orderToReview.OrderNumber));
+        
+        var review = new Review
+        {
+            ReviewerId = command.ReviewerId,
+            OrderId = command.OrderId,
+            Rating = command.Rating,
+            Comment = command.Comment
+        };
+
+        reviewRepo.Add(review);
+        await unitOfWork.SaveAsync(ct);
+
+        return Result.Success();
+    }
+}
